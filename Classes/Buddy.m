98980e6a5594fda22c26538e31954d6d8ddcd712
Filippos Zampounis <zaabalonso@zaabalonso.local>
2016-09-11 08:32:02 +0200

    fixing the bots requests + extracting mock responses to seperate mocks class

--- Classes/Buddy.m
+++ Classes/Buddy.m
@@ -4,11 +4,6 @@
 @implementation Buddy
 @dynamic name, messages;
 
-- (void)dealloc {
-	self.name = nil;
-	self.messages = nil;
-}
-
 - (Message*)lastMessage {
 	for (Message *message in [self.messages reverseObjectEnumerator]) {
 		if (!message.fromMe) {
---
+++ Classes/MockResponses.h
@@ -0,0 +1,15 @@
+//
+//  MockResponses.h
+//  TestTask
+//
+//  Created by Filippos Zampounis on 10/09/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MockResponses : NSObject
+
++(NSString*)RandomPhrase;
+
+@end
